b6d8b46466f3141ef9b049dc2d8539e361a2e71a
Suzuki Taro <t.suzuki@systemconcierge.co.jp>
2023-10-28 08:09:38 +0900

    add Main.kt

---
+++ README.md
@@ -0,0 +1,12 @@
+# poi-kotlin-example
+
+usage:
+
+```
+$ sudo apt install openjdk-17-jre-headless
+$ ./gradlew jar
+$ java -jar build/libs/poi-kotlin-example-1.0-SNAPSHOT.jar test.xlsx
+Sheet1
+a,b
+c,d
+```
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,53 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import org.apache.poi.openxml4j.opc.OPCPackage
+import org.apache.poi.openxml4j.opc.PackageAccess
+import org.apache.poi.ss.usermodel.DataFormatter
+import org.apache.poi.util.XMLHelper
+import org.apache.poi.xssf.eventusermodel.ReadOnlySharedStringsTable
+import org.apache.poi.xssf.eventusermodel.XSSFReader
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler
+import org.apache.poi.xssf.usermodel.XSSFComment
+import org.xml.sax.InputSource
+import java.io.BufferedWriter
+import java.io.File
+import java.io.OutputStreamWriter
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+/**
+ * example of streaming read of .xlsx file using Apache POI
+ *
+ * see also:
+ * - https://poi.apache.org/components/spreadsheet/how-to.html#xssf_sax_api
+ * - https://poi.apache.org/components/spreadsheet/examples.html#xssf-only
+ * - https://svn.apache.org/repos/asf/poi/trunk/poi-examples/src/main/java/org/apache/poi/examples/xssf/eventusermodel/XLSX2CSV.java
+ */
+fun main(args: Array<String>) {
+
+    if (args.isEmpty()) {
+        System.err.println("Usage: java -jar build/libs/poi-kotlin-example-1.0-SNAPSHOT.jar <.xlsx>")
+        return
+    }
+
+    val printer = CSVPrinter(BufferedWriter(OutputStreamWriter(System.out)), CSVFormat.DEFAULT)
+    val xlsxPackage = OPCPackage.open(File(args[0]).path, PackageAccess.READ)
+    val xssfReader = XSSFReader(xlsxPackage)
+    val sheetIterator = xssfReader.sheetsData as XSSFReader.SheetIterator
+    for (inputStream in sheetIterator) {
+        printer.printRecord(sheetIterator.sheetName)
+        XMLHelper.newXMLReader().apply {
+            contentHandler = XSSFSheetXMLHandler(
+                xssfReader.getStylesTable(),
+                null,
+                ReadOnlySharedStringsTable(xlsxPackage),
+                object : XSSFSheetXMLHandler.SheetContentsHandler {
+                    override fun startRow(rowNum: Int) = Unit
+                    override fun endRow(rowNum: Int) = printer.println()
+                    override fun cell(cellReference: String?, formattedValue: String?, comment: XSSFComment?) = printer.print(formattedValue)
+                },
+                DataFormatter(true),
+                false
+            )
+        }.parse(InputSource(inputStream))
+    }
+    printer.flush()
 }
